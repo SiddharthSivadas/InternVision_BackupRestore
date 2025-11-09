e6682e90c084619fffdb6a0f950896f64e8ae6e7
SiddharthSivadas <siddharthsivadas05@gmail.com>
2025-11-09 17:30:18 +0530

    restore.py

--- restore.py
+++ restore.py
@@ -6,9 +6,9 @@ print("🔵 Checking for backups...")
 backup_folder = 'backups'
 
 if not os.path.exists(backup_folder) or len(os.listdir(backup_folder)) == 0:
-    print("⚠️ No backups found to restore.")
+    print(" No backups found to restore.")
 else:
     backups = sorted(os.listdir(backup_folder))
     latest_backup = backups[-1]
     shutil.copy(f"{backup_folder}/{latest_backup}", 'app.db')
-    print(f"✅ Restored {latest_backup} → app.db")
+    print(f" Restored {latest_backup} → app.db")
